c82010b9fb9d424b0fea8cecc45f67411b30e394
1F10230188 <s1f102301886@iniad.org>
2024-07-18 15:13:29 +0900

    issue#17 メモ機能の追加

--- todo/views.py
+++ todo/views.py
@@ -8,7 +8,11 @@ from todo.models import Task
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task = Task(
+            title=request.POST['title'],
+            due_at=make_aware(parse_datetime(request.POST['due_at'])),
+            content=request.POST.get('content', '')
+         )
         task.save()
 
     if request.GET.get('order') == 'due':
@@ -42,6 +46,7 @@ def update(request, task_id):
     if request.method == 'POST':
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
+        task.content = request.POST.get('content', '')
         task.save()
         return redirect(detail, task_id)
 
