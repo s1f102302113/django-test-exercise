6eac061ce9a6d13e460f1b185805954c55e4d789
1F10230196 <s1F102301965@iniad.org>
2024-07-19 11:22:55 +0900

    科目名を詳細ページと更新機能に追加

--- todo/views.py
+++ todo/views.py
@@ -42,6 +42,7 @@ def update(request, task_id):
         raise Http404("Task does not exist")
     if request.method == 'POST':
         task.title = request.POST['title']
+        task.subject = request.POST['subject']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
         return redirect(detail, task_id)
