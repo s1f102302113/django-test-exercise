27b74179b43403a7aa85eb56f46fb3a69a779f10
1F10230188 <s1f102301886@iniad.org>
2024-07-24 00:10:20 +0900

    issue11 更新機能のテストを作成

--- todo/tests.py
+++ todo/tests.py
@@ -113,3 +113,33 @@ class TodoViewTestCase(TestCase):
         response = client.get('/1/')
 
         self.assertEqual(response.status_code, 404)
+
+    def test_update_get(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        response = client.get('/{}/update'.format(task.pk))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/edit.html')
+        self.assertEqual(response.context['task'], task)
+
+    def test_update_post(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        data = {'title': 'Updated Task', 'due_at': '2024-07-01 23:59:59'}
+        response = client.post('/{}/update'.format(task.pk), data)
+
+        self.assertEqual(response.status_code, 302)  # Redirect to detail view
+        updated_task = Task.objects.get(pk=task.pk)
+        self.assertEqual(updated_task.title, 'Updated Task')
+        self.assertEqual(updated_task.due_at, timezone.make_aware(datetime(2024, 7, 1, 23, 59, 59)))
+
+    def test_update_post_invalid_task(self):
+        client = Client()
+        data = {'title': 'Updated Task', 'due_at': '2024-07-01 23:59:59'}
+        response = client.post('/999/update', data)  # Non-existent task
+
+        self.assertEqual(response.status_code, 404)
+        
